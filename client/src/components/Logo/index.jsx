2aba66bb09b135d6cdbe9dfa7f36aea5cd4e732e
Brian <brianpasco1206@gmail.com>
2025-09-15 14:37:53 +0800

    feat(public & components): add logo image in Logo component

--- client/src/components/Logo/index.jsx
+++ client/src/components/Logo/index.jsx
@@ -1,14 +1,19 @@
 import styles from './Logo.module.css';
 import { useNavigate } from 'react-router';
+import { useTheme } from '@contexts';
 
 const Logo = () => {
 
+    const { theme } = useTheme();
     const navigate = useNavigate();
 
     return (
         <div className={ styles['logo'] } onClick={ () => navigate('/') }>
-            <h2 className={ styles['logo-left'] }>Seraphim</h2>
-            <h2 className={ styles['logo-right'] }>Luxe</h2>
+            <img className={ styles['logo-image'] } src={ `../../../assets/seraphim-luxe-logo-${ theme }.png` } alt={ `Seraphim Luxe logo for ${ theme } theme.` } />
+            <div className={ styles['logo-text'] }>
+                <h2 className={ styles['logo-text-left'] }>Seraphim</h2>
+                <h2 className={ styles['logo-text-right'] }>Luxe</h2>
+            </div>
         </div>
     );
 };
