413ba717df419bffddd4ce7db7cbb5f0c503b55f
Brian <brianpasco1206@gmail.com>
2025-09-16 22:33:27 +0800

    fix(components): use uploaded image instead of assets in Logo component

--- client/src/components/Logo/index.jsx
+++ client/src/components/Logo/index.jsx
@@ -6,10 +6,13 @@ const Logo = () => {
 
     const { theme } = useTheme();
     const navigate = useNavigate();
+    const logoImageUrl = theme === 'dark'
+        ? 'https://res.cloudinary.com/dfvy7i4uc/image/upload/seraphim-luxe-logo-dark_ca5azw.png'
+        : 'https://res.cloudinary.com/dfvy7i4uc/image/upload/seraphim-luxe-logo-light_emmoup.png'
 
     return (
         <div className={ styles['logo'] } onClick={ () => navigate('/') }>
-            <img className={ styles['logo-image'] } src={ `../../../assets/seraphim-luxe-logo-${ theme }.png` } alt={ `Seraphim Luxe logo for ${ theme } theme.` } />
+            <img className={ styles['logo-image'] } src={ logoImageUrl } alt={ `Seraphim Luxe logo for ${ theme } theme.` } />
             <div className={ styles['logo-text'] }>
                 <h2 className={ styles['logo-text-left'] }>Seraphim</h2>
                 <h2 className={ styles['logo-text-right'] }>Luxe</h2>
