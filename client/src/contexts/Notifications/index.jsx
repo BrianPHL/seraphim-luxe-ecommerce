3d652cce5e2907cb5b2ec6632211d62dffb49ff0
Brian <brianpasco1206@gmail.com>
2025-09-21 20:56:24 +0800

    feat(contexts): add refactored fetch notifications logic in Notifications context

--- client/src/contexts/Notifications/index.jsx
+++ client/src/contexts/Notifications/index.jsx
@@ -5,10 +5,65 @@ import NotificationsContext from "./context";
 
 export const NotificationsProvider = ({ children }) => {
 
+    const { user } = useAuth();
+    const { showToast } = useToast();
+    const [ notifications, setNotifications ] = useState([]);
+    const [ isInboxOpen, setIsInboxOpen ] = useState(false);
+    const [ unreadCount, setUnreadCount ] = useState(0);
+
+    const fetchNotifications = useCallback(async () => {
+
+        if (!user) return;
+
+        try {
+            
+            const response = await fetchWithTimeout(`/api/notifications/${ user.id }`, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+            if (!response.ok)
+                throw new Error("Failed to fetch notifications!");
+
+            const data = await response.json();
+
+            setNotifications(data || []);
+
+        } catch (err) {
+
+            console.error("Notifications context fetchNotifications function error: ", err);
+            showToast('Failed to retrieve notifications!', 'error')
+            
+        }
+    }, [ user ]);
+
+    const fetchUnreadCount = useCallback(() => {
+
+        const unreadCount = notifications.filter(notification => notification.is_read === 0).length;
+
+        setUnreadCount(unreadCount);
+
+    }, [ notifications ])
+
+
+    useEffect(() => {
+        fetchNotifications()
+    }, [ user ]);
+
+    useEffect(() => {
+        fetchUnreadCount()
+    }, [ notifications ])
 
     return (
         <NotificationsContext.Provider value={{
             
+        // * Data
+            notifications,
+            unreadCount,
+        // * Exposed functions
+            fetchNotifications,
+            setIsInboxOpen
+
         }}>
             { children }
         </NotificationsContext.Provider>
